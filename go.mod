5c912911fa57cef7260dae7ab78d28ae31238d55
Adrien MOREAU <moreauadrien92@gmail.com>
2022-08-01 23:17:06 +0200

    v1

--- go.mod
+++ go.mod
@@ -6,12 +6,31 @@ require github.com/GoogleCloudPlatform/functions-framework-go v1.5.3
 
 require (
 	cloud.google.com/go/functions v1.0.0 // indirect
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/antchfx/htmlquery v1.2.5 // indirect
+	github.com/antchfx/xmlquery v1.3.12 // indirect
+	github.com/antchfx/xpath v1.2.1 // indirect
 	github.com/cloudevents/sdk-go/v2 v2.6.1 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.1.2 // indirect
+	github.com/jawher/mow.cli v1.2.0 // indirect
+	github.com/jomei/notionapi v1.8.5 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/net v0.0.0-20220728211354-c7608f3a8462 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- saveit_http.go
+++ saveit_http.go
@@ -1,16 +1,182 @@
 package function
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/PuerkitoBio/goquery"
 )
 
-func init() {
-	functions.HTTP("HelloWorld", helloWorld)
+type PageDetails struct {
+	title   string
+	image   string
+	favicon string
+	url     string
 }
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+type ReceivedPayload struct {
+	IntegrationToken string `json:"integrationToken"`
+	DatabaseId       string `json:"databaseId"`
+	Url              string `json:"url"`
+	Tags             string `json:"tags"`
+}
+
+func init() {
+	functions.HTTP("SaveIt", saveIt)
+}
+
+func formatTags(rawTags string) string {
+	tags := strings.Split(rawTags, ",")
+	for i := 0; i < len(tags); i++ {
+		tag := strings.Trim(tags[i], " ")
+		tags[i] = `{"name":"` + tag + `"}`
+	}
+
+	return "[" + strings.Join(tags, ",") + "]"
+}
+
+func getPageDetails(u string) (PageDetails, error) {
+	details := PageDetails{}
+
+	res, err := http.Get(u)
+	if err != nil {
+		return details, fmt.Errorf("The page could not be fetched")
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return details, fmt.Errorf("The page return a %d error", res.StatusCode)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return details, err
+	}
+
+	favicon, err := getFaviconUrl(u)
+	if err != nil {
+		return details, err
+	}
+
+	details.favicon = favicon
+
+	image, _ := doc.Find("meta[property='og:image']").Attr("content")
+	details.image = image
+
+	title := doc.Find("title").Text()
+	details.title = title
+
+	details.url = u
+
+	return details, nil
+}
+
+func getFaviconUrl(u string) (string, error) {
+	o, err := url.Parse(u)
+	if err != nil {
+		return "", err
+	}
+
+	host := strings.Split(o.Host, ":")[0]
+
+	return "https://icon.horse/icon/" + host, nil
+}
+
+func getNotionPayload(details PageDetails, tags string, rPayload ReceivedPayload) *bytes.Buffer {
+	pageContent := ""
+	if len(details.image) > 0 {
+		pageContent = `"children": [
+                {
+                    "object": "block",
+                    "image": {
+                        "external": {
+                            "url": "` + details.image + `"
+                        }
+                    }
+                    
+                }
+            ],`
+	}
+	notionPayload := []byte(`{
+        "parent": {
+            "database_id": "` + rPayload.DatabaseId + `"
+        },
+        "icon": {
+            "external": {
+                "url": "` + details.url + `"
+            }
+        },
+        ` + pageContent + `
+        "properties": {
+            "Name": {
+                "title": [
+                    {
+                        "text": {
+                            "content": "` + details.title + `"
+                        }
+                    }
+                ]
+            },
+            "Tags": {
+                "multi_select": ` + tags + `
+            },
+            "URL": {
+                "url": "` + details.url + `"
+            }
+
+        }
+    }`)
+
+	return bytes.NewBuffer(notionPayload)
+}
+
+func saveIt(w http.ResponseWriter, r *http.Request) {
+	d := ReceivedPayload{}
+
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		http.Error(w, "{\"error\": \"The body could not be parsed\"}", http.StatusBadRequest)
+		return
+	}
+
+	tags := formatTags(d.Tags)
+
+	details, err := getPageDetails(d.Url)
+
+	if err != nil {
+		http.Error(w, "{\"error\": \""+err.Error()+"\"}", http.StatusBadRequest)
+		return
+	}
+
+	notionPayload := getNotionPayload(details, tags, d)
+
+	fmt.Println(notionPayload.String())
+
+	req, err := http.NewRequest("POST", "https://api.notion.com/v1/pages", notionPayload)
+	req.Header.Set("Authorization", "Bearer "+d.IntegrationToken)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Notion-Version", "2022-06-28")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		http.Error(w, "{\"error\": \""+err.Error()+"\"}", http.StatusBadRequest)
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	if resp.StatusCode != 200 {
+		http.Error(w, string(body), http.StatusBadRequest)
+	}
+
+	fmt.Fprintf(w, string(body))
 }
